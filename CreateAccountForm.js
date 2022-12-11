8c1c28557ff3dd703b09f7f48ed6183abb5075c7
Brendan Jarmusz <brendan@jarmusz.com>
2022-12-11 13:24:43 -0500

    Added CreateAccountForm.js
    Added account creation

---
+++ CreateAccountForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const CreateAccountForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Validate form data and create account
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Create Account</button>
+    </form>
+  );
+};
+
+export default CreateAccountForm;
